ddc38bc51623abf302b26be4caf633ce11b655e9
Alejandro Larios <alexlarios70@hotmail.com>
2024-06-05 00:11:47 -0600

    added file for the tag routes, and added the entry point for tags on the index

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router()
 const user = require ("./userRoutes")
 const favorites = require ("./favoriteRoutes")
+const tags = require ("./tagRoutes")
 
 router.use("/user",user)
 router.use("/favorites",favorites)
+router.use("/tags", favorites)
 
 module.exports = router
